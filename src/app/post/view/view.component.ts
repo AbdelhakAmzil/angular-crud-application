7f4dd73c54b2fe863b3399b008e92facd759e93d
Abdelhak AMZIL <abdelhakamzil.2000@gmail.com>
2026-05-30 10:31:32 +0100

    create routes + interface + Service

---
+++ src/app/post/view/view.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view.component',
+  imports: [],
+  templateUrl: './view.component.html',
+  styleUrl: './view.component.css',
+})
+export class ViewComponent {}
